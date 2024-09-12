1ad972cd32fcc5751c87119ed83c795c677634ac
cocoa <95043086+hotcocoaNcode@users.noreply.github.com>
2024-09-12 14:49:15 -0500

    Fix error

--- prime-error.cpp
+++ prime-error.cpp
@@ -5,25 +5,25 @@ int main()
     //Math
     for(int num = 1; num <= 100; num++)
     {
-        int primality = 0; 
+        int primality = 0;
         for(int mod = 2; mod <= num/2; mod++)
         {
-            //Modulio 
+            //Modulio
             if(num % mod == 0)
             {
                 primality += 1;
             }
-            
+
         }
         //Prime Checker
         if(primality > 0)
         {
-            std::cout << (num + "Is Composite") << std::endl;
+            std::cout << (std::to_string(num) + " is Composite") << std::endl;
         }
 
         else
         {
-            std::cout << (num + "Is Prime") << std::endl;
+            std::cout << (std::to_string(num) + " is Prime") << std::endl;
         }
     }
 }
